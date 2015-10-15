e1614578b472be5980e8a43292f85bd5fa06caff
li-pc-2 <limingwei@mail.com>
2015-10-15 21:28:36 +0800

    SummerJUnit4Runner

--- src/main/java/summer/junit/SummerJUnit4Runner.java
+++ src/main/java/summer/junit/SummerJUnit4Runner.java
@@ -1,7 +1,14 @@
 package summer.junit;
 
+import java.lang.reflect.Field;
+import java.util.List;
+
 import org.junit.runners.BlockJUnit4ClassRunner;
 import org.junit.runners.model.InitializationError;
+
+import summer.ioc.IocContext;
+import summer.ioc.annotation.Inject;
+import summer.util.Reflect;
 
 /**
  * @author li
@@ -10,12 +17,32 @@ import org.junit.runners.model.InitializationError;
  * @see org.springframework.test.context.junit4.SpringJUnit4ClassRunner
  */
 public class SummerJUnit4Runner extends BlockJUnit4ClassRunner {
+    private static IocContext iocContext;
+
     public SummerJUnit4Runner(Class<?> klass) throws InitializationError {
         super(klass);
     }
 
+    public synchronized IocContext getIocContext() {
+        try {
+            if (null == iocContext) {
+                iocContext = (IocContext) getClass().getMethod("getIocContext").invoke(this);
+            }
+            return iocContext;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     protected Object createTest() throws Exception {
         Object testInstance = super.createTest();
+        List<Field> fields = Reflect.getDeclaredFields(getTestClass().getJavaClass());
+        for (Field field : fields) {
+            Inject inject = field.getAnnotation(Inject.class);
+            if (null != inject) {
+                Reflect.setFieldValue(this, field, getIocContext().getBean(field.getType(), inject.value()));
+            }
+        }
         return testInstance;
     }
 }
